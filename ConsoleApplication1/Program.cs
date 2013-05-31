29bfccc632da41b00819545e7b35a2e5325b9cec
Bob Millar <bobm@ngsnow.com>
2013-05-31 13:00:49 -0700

    Initial Town Crier Example Using Console.WriteLine

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //HostFactory.New(x => x.StartAutomatically());
+
+            HostFactory.Run(x =>
+                                {
+                                    x.Service<TownCrier> (s =>
+                                        {
+                                            s.ConstructUsing(name => new TownCrier());
+                                            s.WhenStarted(tc => tc.Start());
+                                            s.WhenStopped(tc => tc.Stop());
+                                        });
+                                    x.RunAsLocalSystem();
+                                    x.SetDescription("Sample Topshelf Host");
+                                    x.SetDisplayName("Stuff");
+                                    x.SetServiceName("Stuff");
+                                });
+        }
+    }
+}
